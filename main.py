9f778b32c3cb8eed9b95e53fa1b9a77b6bb6eda4
Mikołaj Pokrywka <mp7961@gmail.com>
2023-01-17 22:29:38 +0100

    cros origin

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ from extract import extract
 from typing import Union
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/invoice")
 async def create_file(file: UploadFile = File()):
